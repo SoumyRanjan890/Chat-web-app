38f86cae75cc96c49f13e519ad7b4cc6829a991b
SoumyRanjan890 <122106462+SoumyRanjan890@users.noreply.github.com>
2023-10-03 18:44:41 +0530

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,44 @@
+const socket = io();
+const clientstotal =document.querySelector(".clients-total")
+const clientsTotal = document.getElementById('client-total')
+const messageContainer = document.getElementById('message-container')
+const nameInput = document.getElementById('name-input')
+const messageForm = document.getElementById('message-form')
+const messageInput = document.getElementById('message-input') 
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    sendMessage()
+    })
+
+socket.on('clients-total', (data) => {
+    clientstotal.innerText = `Total Clients: ${data}`
+ })
+
+ function sendMessage() {
+    console.log(messageInput.value)
+    const data = {
+    name: nameInput.value,
+    message: messageInput.value,
+    dateTime: new Date()
+    } 
+    socket.emit('message', data)
+    addMessageToUI(true,data)
+    messageInput.value=''
+ }
+ socket.on('chat-message', (data) => {
+    console.log(data)
+    addMessageToUI(false,data)
+    })
+ function addMessageToUI(isOwnMessage, data){
+    const element =`
+    <li class="${isOwnMessage ? 'message-right':'message-left'}">
+    <p class="message">
+            ${data.message}
+    </p>
+    </li>
+    `
+    messageContainer.innerHTML += element
+
+}
+
